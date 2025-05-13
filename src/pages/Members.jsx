1f7586cd8d10b2dc2953eaac3875d25b00ec3161
Muhammad Annas Khan <annasking601@gmail.com>
2025-05-13 15:03:42 +0500

    Fixes

--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import logo0 from "../assets/Logo0.png";
+import logo1 from "../assets/Logo1.png";
+import logo2 from "../assets/Logo2.png";
+import logo3 from "../assets/Logo3.png";
+import logo4 from "../assets/Logo4.jpg";
 
 const Members = () => {
   return (
@@ -13,27 +18,27 @@ const Members = () => {
       <div className="flex flex-wrap items-center justify-center gap-6 mb-4">
         <img
           className="w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 max-w-xs object-contain"
-          src="src\assets\Logo 0.png"
+          src={logo0}
           alt="BIID Member"
         />
         <img
           className="w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 max-w-xs object-contain"
-          src="src\assets\Logo 4.jpg"
+          src={logo4}
           alt="Kingston Chamber Logo"
         />
         <img
           className="w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 max-w-xs object-contain"
-          src="src\assets\Logo 1.PNG"
+          src={logo1}
           alt="Logo 1"
         />
         <img
           className="w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 max-w-xs object-contain"
-          src="src\assets\Logo 2.PNG"
+          src={logo2}
           alt="Logo 2"
         />
         <img
           className="w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 max-w-xs object-contain"
-          src="src\assets\Logo 3.PNG"
+          src={logo3}
           alt="Logo 3"
         />
       </div>
